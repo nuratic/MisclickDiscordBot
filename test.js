b876b2b81714fb16a86a0bc743cb652df03f4345
dnorfolk2 <d.norfolk2627@student.leedsbeckett.ac.uk>
2021-10-04 22:13:08 +0100

    Added test.js to test Twitch API with nodejs

---
+++ test.js
@@ -0,0 +1,87 @@
+const  request  = require('request');
+const { twitchClientId, twitchClientSecret } = require('./config.json');
+
+const getToken = (url, callback) => {
+    const options =  {
+        url: "https://id.twitch.tv/oauth2/token",
+        json: true,
+        body: {
+            client_id: twitchClientId,
+            client_secret: twitchClientSecret,
+            grant_type: "client_credentials"
+        }
+    };
+
+    request.post(options, (err, res, body) => {
+        if (err) {
+            return console.log(err);
+        }
+        // console.log(`Status: ${res.statusCode}`);
+        // console.log(body);
+
+        callback(res);
+    });
+};
+
+let AT = "";
+getToken("https://id.twitch.tv/oauth2/token", (res) => {
+    // console.log(res.body)
+    AT = res.body.access_token;
+    return AT;
+})
+
+setTimeout(() => {
+    const getUser = (url, callback) => {
+        const options =  {
+            url: "https://api.twitch.tv/helix/users?login=briziana",
+            json: true,
+            headers: {
+                "Authorization": `Bearer ${AT}`,
+                "Client-Id": twitchClientId
+            }
+        };
+    
+        request.get(options, (err, res, body) => {
+            if (err) {
+                return console.log(err);
+            }
+            // console.log(`Status: ${res.statusCode}`);
+            // console.log(body);
+    
+            callback(res);
+        });
+    };
+    let bID = "";
+    getUser("https://api.twitch.tv/helix/users?login=briziana", (res) => {
+        // console.log(res.body['data'][0]['id'])
+        bID = res.body['data'][0]['id'];
+        return bID
+    })
+
+    setTimeout(() => {
+        const getSchedule = (url, callback) => {
+            const options =  {
+                url: `https://api.twitch.tv/helix/schedule?broadcaster_id=${bID}&first=7`,
+                json: true,
+                headers: {
+                    "Authorization": `Bearer ${AT}`,
+                    "Client-Id": twitchClientId
+                }
+            };
+        
+            request.get(options, (err, res, body) => {
+                if (err) {
+                    return console.log(err);
+                }
+                // console.log(`Status: ${res.statusCode}`);
+                // console.log(body);
+        
+                callback(res);
+            });
+        };
+        
+        getSchedule(`https://api.twitch.tv/helix/schedule?broadcaster_id=${bID}&first=10`, (res) => {
+            console.log(res.body['data']['segments'])
+        })
+    }, 1000)
+}, 1000);
